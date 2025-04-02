5b2b7c3fd72e6877c96182a5bbdf995ac4f69104
Aleksandr <dipperpryes@mail.ru>
2025-04-02 22:27:44 +0300

    updated factory

--- GraphicEditor/ShapesFactory/ShapeFactory.cs
+++ GraphicEditor/ShapesFactory/ShapeFactory.cs
@@ -9,10 +9,13 @@ namespace GraphicEditor
 {
     public class ShapeFactory
     {
-        public Dictionary<string, Func<Point, Shape>> InitializeShapeFactory(Button penColorButton, Button brushColorButton, TrackBar widthTrackBar, TrackBar countTrackBar)
+        public Dictionary<string, Func<Point, Shape>> InitializeShapeFactory(
+            Button penColorButton,
+            Button brushColorButton,
+            TrackBar widthTrackBar,
+            TrackBar countTrackBar)
         {
             var shapeFactories = new Dictionary<string, Func<Point, Shape>>();
-
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
             var shapeClasses = assemblies
@@ -26,36 +29,54 @@ namespace GraphicEditor
 
                 Func<Point, Shape> shapeFactory = (startPos) =>
                 {
-                    ConstructorInfo constructor = shapeClassType.GetConstructors().First(); 
+                    ConstructorInfo constructor = shapeClassType.GetConstructors().First();
                     if (constructor == null)
                         throw new InvalidOperationException($"No constructor found for {shapeClassType.Name}");
 
                     ParameterInfo[] parameters = constructor.GetParameters();
+
+                    var typeResolvers = new Dictionary<Type, Func<ParameterInfo, object>>
+                    {
+                        [typeof(Point)] = _ => startPos,
+                        [typeof(Color)] = (p) =>
+                        {
+                            var colorParams = parameters.Where(x => x.ParameterType == typeof(Color)).ToList();
+                            int colorIndex = colorParams.IndexOf(p);
+
+                            return colorIndex switch
+                            {
+                                0 => penColorButton.BackColor,
+                                1 => brushColorButton.BackColor,
+                                _ => penColorButton.BackColor 
+                            };
+                        },
+                        [typeof(int)] = (p) =>
+                        {
+                            var intParams = parameters.Where(x => x.ParameterType == typeof(int)).ToList();
+                            int paramIndex = intParams.IndexOf(p);
+
+                            return paramIndex switch
+                            {
+                                0 => widthTrackBar.Value,
+                                1 => countTrackBar.Value,
+                                _ => 0
+                            };
+                        }
+                    };
+
                     object[] args = new object[parameters.Length];
 
-                    foreach (var param in parameters)
+                    for (int i = 0; i < parameters.Length; i++)
                     {
-                        if (param.ParameterType == typeof(Color))
+                        var param = parameters[i];
+                        if (typeResolvers.TryGetValue(param.ParameterType, out var resolver))
                         {
-                            if (args.Contains(penColorButton.BackColor))
-                                args[param.Position] = brushColorButton.BackColor;
-                            else
-                                args[param.Position] = penColorButton.BackColor;
-                        }
-                        else if (param.ParameterType == typeof(int))
-                        {
-                            if (args.Contains(widthTrackBar.Value))
-                                args[param.Position] = countTrackBar.Value;
-                            else
-                                args[param.Position] = widthTrackBar.Value;
-                        }
-                        else if (param.ParameterType == typeof(Point))
-                        {
-                            args[param.Position] = startPos;
+                            args[i] = resolver(param);
                         }
                         else
                         {
-                            throw new InvalidOperationException($"Undefined parameter type: {param.ParameterType.Name} in {shapeClassType.Name}");
+                            throw new InvalidOperationException(
+                                $"Undefined parameter type: {param.ParameterType.Name} in {shapeClassType.Name}");
                         }
                     }
 
